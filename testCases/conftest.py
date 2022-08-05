10faa80203ddefdb90522bb6cf1e50c8c542892a
mahimine77@gmail.com <minemahi77>
2022-08-05 21:40:54 +0530

    git add NonCommerceApp

---
+++ testCases/conftest.py
@@ -0,0 +1,39 @@
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture()
+def setup(browser):
+    global driver
+    if browser == 'chrome':
+        driver = webdriver.Chrome(".\\Driver/chromedriver_win32/chromedriver.exe")
+        print("Launching chrome browser.........")
+    elif browser == 'firefox':
+        driver = webdriver.Firefox("Driver/geckodriver-v0.31.0-win64/geckodriver.exe")
+        print("Launching firefox browser.........")
+    return driver
+
+
+def pytest_addoption(parser):  # This will get the value from CLI /hooks
+    parser.addoption("--browser")
+
+
+@pytest.fixture()
+def browser(request):  # This will return the Browser value to setup method
+    return request.config.getoption("--browser")
+
+
+########### pytest HTML Report ################
+
+# It is hook for Adding Environment info to HTML Report
+def pytest_configure(config):
+    config._metadata['Project Name'] = 'nop Commerce'
+    config._metadata['Module Name'] = 'Customers'
+    config._metadata['Tester'] = 'Mahesh'
+
+
+# It is hook for delete/Modify Environment info to HTML Report
+@pytest.mark.optionalhook
+def pytest_metadata(metadata):
+    metadata.pop("JAVA_HOME", None)
+    metadata.pop("Plugins", None)
